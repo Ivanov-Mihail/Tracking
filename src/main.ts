0a8989da131e06274394cc754d48caf382cf2978
Михаил Иванов <ivanov-mihail-3@live.com>
2020-05-07 14:47:20 +0300

    added HybridApp and packages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const microservice = app.connectMicroservice({
+  transport: Transport.TCP,
+});
+
+await app.startAllMicroservicesAsync();
+await app.listen(3001);
 }
 bootstrap();
