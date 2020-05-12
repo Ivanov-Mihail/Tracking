42c92fab0b4c207f8b0f9430b54d323dd6d09ab6
Михаил Иванов <ivanov-mihail-3@live.com>
2020-05-12 12:53:16 +0300

    fixes

--- src/redis/redis.controller.ts
+++ src/redis/redis.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Post, Body, Get, Param, Logger, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Logger,
+  Delete,
+} from '@nestjs/common';
 import { DbService } from 'src/db/db.service';
 import { PublisherPositionDTO } from 'src/db/dto/publisher-pos-dto';
 import { Publisher } from 'src/db/interfaces/publisher.inteface';
@@ -13,44 +21,43 @@ export class GeolocationController {
 
   private readonly logger = new Logger('RedisController', false);
 
-  @Get('/publisher') // localhost:3001/publisher
+  @Get('/publisher') // localhost:3001/geolocation/publisher
   async GetAllPublishers(): Promise<Publisher[]> {
     this.logger.log(`Request GET: publisher - ${this.timeNow}`);
     return this.dbService.GetAllPublishers();
   }
 
-  @Get('publisher/:id') // localhost:3001/publisher/5eb564ac92fc1c15344586bc
+  @Get('publisher/:id') // localhost:3001/geolocation/publisher/5eb564ac92fc1c15344586bc
   async GetPublisherById(@Param('id') id: string): Promise<Publisher> {
     this.logger.log(`Request GET: publisher/${id} - ${this.timeNow}`);
     return this.dbService.GetPublisherById(id);
   }
 
-  @Post('/publisher') //localhost:3001/publisher
-  CreatePublisher(@Body() publisher: PublisherPositionDTO) {
-    this.dbService.AddPublsiher(publisher);
+  @Post('/publisher') //localhost:3001/geolocation/publisher
+  async CreatePublisher(@Body() publisher: PublisherPositionDTO) {
+    await this.dbService.AddPublsiher(publisher);
   }
 
-  @Post('/point')  //localhost:3001/point
-  CreatePoint(@Body() point: GeoPointDTO) {
-    this.dbService.AddPoint(point).then();
+  @Post('/point') //localhost:3001/geolocation/point
+  async CreatePoint(@Body() point: GeoPointDTO) {
+    await this.dbService.AddPoint(point).then();
+    console.log();
   }
 
-
-  @Get('/subscriber') //localhost:3001/subscriber
-  async GetSubscriber(@Param('id') publisherId: string) : Promise<Subscriber[]>{
-    const subscribers = this.dbService.GetSubscribers(publisherId);
+  @Get('/subscriber') //localhost:3001/geolocation/subscriber
+  async GetSubscriber(@Param('id') publisherId: string): Promise<Subscriber[]> {
+    const subscribers = await this.dbService.GetSubscribers(publisherId);
     return subscribers;
   }
 
-  @Post('/subscriber') //localhost:3001/subscriber
-  CreateSubscriber(@Body() subscriberDTO: SubscriberDTO) {
-    this.dbService.AddSubscriber(subscriberDTO);
+  @Post('/subscriber') //localhost:3001/geolocation/subscriber
+  async CreateSubscriber(@Body() subscriberDTO: SubscriberDTO) {
+    await this.dbService.AddSubscriber(subscriberDTO);
   }
 
-
   @Delete('/subscriber')
-  DeleteSubscriber(@Body() subscriberDTO: SubscriberDTO) {
-    this.dbService.DeleteSubscriber(subscriberDTO);
+  async DeleteSubscriber(@Body() subscriberDTO: SubscriberDTO) {
+    await this.dbService.DeleteSubscriber(subscriberDTO);
   }
 
   /* ****************************************************************************** 
